529c1f852b12962c29cb3468221faa314b59ef16
Metanha <110100017+Metanha@users.noreply.github.com>
2025-02-10 23:08:41 +0000

    Update my_scrap_app.py

--- my_scrap_app.py
+++ my_scrap_app.py
@@ -7,6 +7,17 @@ from requests import get
 from bs4 import BeautifulSoup as bs
 import glob
 #--------------------------------------------------------------------------
+#Charger les donnéés
+def load_(dataframe, title):
+    st.markdown("""
+    <style>
+    div.stButton {text-align:center}
+    </style>""", unsafe_allow_html=True)
+
+    #if st.button(title, key):
+    st.subheader('Display data dimension')
+    st.write('Data dimension: ' + str(dataframe.shape[0]) + ' lignes et ' + str(dataframe.shape[1]) + ' colonnes.')
+    st.dataframe(dataframe)
 
 ############## CODE SCRAPING DES ORDINATEURS
 def scrape_data_ordin(url):
@@ -31,13 +42,61 @@ def scrape_data_ordin(url):
             d=pd.DataFrame({"Details":[details],"Etat":[Etat],"Marque":[Marque],"Addresse":[Addresse],"Prix":[Prix],"Lien_image":[Lien_image]})
             
             data=pd.concat([data,d],ignore_index=True)
-            return data
         except Exception as e:
             print(f"Erreur lors de l'extraction d'un contenu : {e}")
-            return None
+    return data
 
+###########SCRAPPER LES TELEPHONES
+def scrape_data_telep(url):
+    res=get(url) # récupère le code HTML de la page
+    soup=bs(res.text,"html.parser") #stocker le code html dans un objet beautifulSoup ,
+    contenairs=soup.find_all("div",class_="listing-card__content 1")
+    
+    data=pd.DataFrame(columns=["Details","Etat","Marque","Addresse","Prix","Lien_image"])
+    
+    for content in contenairs:
+        try:
+            details=content.find("div",class_="listing-card__header__title").text.replace("\n","").replace("  ","")
+            Etat=content.find("div",class_="listing-card__header__tags").find_all("span")[0].text
+            Marque=content.find("div",class_="listing-card__header__tags").find_all("span")[1].text
+            Addresse=content.find("div",class_="listing-card__header__location").text.replace("\n","").replace("  ","")
+            Prix=content.find("div",class_="listing-card__info-bar__price").find("span",class_="listing-card__price__value 1").text.replace("\n","").replace("F Cfa","").replace("\u202f","").replace(" ","")
+            Lien_image=content.find_all("img", class_="listing-card__image__resource vh-img") #["src"]
+    
+            d=pd.DataFrame({"Details":[details],"Etat":[Etat],"Marque":[Marque],"Addresse":[Addresse],"Prix":[Prix],"Lien_image":[Lien_image]})
+            
+            data=pd.concat([data,d],ignore_index=True)
+    
+        except Exception as e:
+            print(f"Erreur lors de l'extraction d'un contenu : {e}")
+    return data
 
-# Configuration de la page , layout="black"
+########## SCRAPER LES TELEVISION
+def scrape_data_tele(url):
+    res=get(url) # récupère le code HTML de la page
+    soup=bs(res.text,"html.parser") #stocker le code html dans un objet beautifulSoup ,
+    contenairs=soup.find_all("div",class_="listing-card__content 1")
+
+    data=pd.DataFrame(columns=["Details","Etat","Marque","Addresse","Prix","Lien_image"])
+
+    for content in contenairs:
+        try:
+            details=content.find("div",class_="listing-card__header__title").text.replace("\n","").replace("  ","")
+            Etat=content.find("div",class_="listing-card__header__tags").find_all("span")[0].text
+            Marque=content.find("div",class_="listing-card__header__tags").find_all("span")[1].text
+            Addresse=content.find("div",class_="listing-card__header__location").text.replace("\n","").replace("  ","")
+            Prix=content.find("div",class_="listing-card__info-bar__price").find("span",class_="listing-card__price__value 1").text.replace("\n","").replace("F Cfa","").replace("\u202f","").replace(" ","")
+            Lien_image=content.find_all("img", class_="listing-card__image__resource vh-img") #["src"]
+    
+            d=pd.DataFrame({"Details":[details],"Etat":[Etat],"Marque":[Marque],"Addresse":[Addresse],"Prix":[Prix],"Lien_image":[Lien_image]})
+            
+            data=pd.concat([data,d],ignore_index=True)
+    
+        except Exception as e:
+            print(f"Erreur lors de l'extraction d'un contenu : {e}{page}")
+    return data
+
+# Configuration de la page 
 st.set_page_config(page_title="Web Scraping App")
 
 # Barre latérale pour la navigation
@@ -66,25 +125,14 @@ if menu == "📊 Scraper des données":
             df=scrape_data_ordin(url)
         elif categorie=="Téléphones":
             url="https://www.expat-dakar.com/telephones?page=1"
-            df=scrape_data_ordin(url)
+            df=scrape_data_telep(url)
         elif categorie=="Télévision":
             url="hhttps://www.expat-dakar.com/tv-home-cinema?page=1"
-            df=scrape_data_ordin(url)
+            df=scrape_data_tele(url)
     
      #Telecharger les données scrappées  
     if telecharger_donne:
         csv = df.to_csv(path_or_buf="data/donnees_scrapes.csv",index=False).encode('utf-8')
-        
-def load_(dataframe, title):
-    st.markdown("""
-    <style>
-    div.stButton {text-align:center}
-    </style>""", unsafe_allow_html=True)
-
-    #if st.button(title, key):
-    st.subheader('Display data dimension')
-    st.write('Data dimension: ' + str(dataframe.shape[0]) + ' lignes et ' + str(dataframe.shape[1]) + ' colonnes.')
-    st.dataframe(dataframe)
 
 
 #chargement des données
