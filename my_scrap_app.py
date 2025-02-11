5af934a1873e486b8e2199abaed32f5821dfafc7
Metanha <110100017+Metanha@users.noreply.github.com>
2025-02-11 21:58:03 +0000

    Update my_scrap_app.py

--- my_scrap_app.py
+++ my_scrap_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import glob
-import os
 
 
 st.markdown("<h1 style='text-align: center; color: black;'>MY DATA APP</h1>", unsafe_allow_html=True)
@@ -12,32 +10,38 @@ This app allows you to download scraped data on motocycles from expat-dakar
 * **Data source:** [Expat-Dakar](https://www.expat-dakar.com/).
 """)
 
-def load_(dataframe, title, key):
+
+# Fonction de loading des données
+def load_(dataframe, title, key) :
     st.markdown("""
     <style>
     div.stButton {text-align:center}
     </style>""", unsafe_allow_html=True)
 
-    if st.button(title, key):
+    if st.button(title,key):
+      
         st.subheader('Display data dimension')
         st.write('Data dimension: ' + str(dataframe.shape[0]) + ' rows and ' + str(dataframe.shape[1]) + ' columns.')
         st.dataframe(dataframe)
 
-
+# définir quelques styles liés aux box
 st.markdown('''<style> .stButton>button {
     font-size: 12px;
     height: 3em;
     width: 25em;
 }</style>''', unsafe_allow_html=True)
 
+          
+# Charger les données 
+load_(pd.read_csv('data/motos_scooters1.csv'), 'Motocycles data 1', '1')
+load_(pd.read_csv('data/motos_scooters2.csv'), 'Motocycles data 2', '2')
+load_(pd.read_csv('data/motos_scooters3.csv'), 'Motocycles data 3', '3')
+load_(pd.read_csv('data/motos_scooters4.csv'), 'Motocycles data 4', '4')
+load_(pd.read_csv('data/motos_scooters5.csv'), 'Motocycles data 5', '5')
 
-load_(pd.read_excel('data/P1_Ordinateurs.xlsx'), 'Ordinateurs', '1')
-load_(pd.read_excel('data/P1_Telephones.xlsx'), 'Téléphones', '2')
-load_(pd.read_excel('data/P1_cinema.xlsx'), 'Télévision', '3')
 
 
-#range(len(glob.glob("data/*.xlsx"))),
-for i,f in enumerate(glob.glob("data/*.xlsx")):
-    df =pd.read_excel(f)
-    filename = os.path.basename(f).replace(".xlsx", "")  # Extraire le nom du fichier sans extension
-    load_(df, filename, str(i+1))
+
+ 
+
+
